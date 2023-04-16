76cb6b66e9e7954f9cd6fdab891bf79ae8aad410
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-04-16 13:06:36 +0800

    first

--- desktop_main.cpp
+++ desktop_main.cpp
@@ -1,52 +1,67 @@
-#include <windows.h>
+#include <Windows.h>
 
+// Deklarasi fungsi callback untuk menangani pesan-pesan yang diterima oleh jendela aplikasi
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
-
-        default:
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
-    }
-    return 0;
-}
-
+// Fungsi utama aplikasi
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    LPCWSTR szClassName = L"MyWindowClass";
-    LPCWSTR szWindowName = L"My Window";
-    HWND hwnd;
-    MSG msg;
+    // Nama kelas jendela
+    LPCSTR className = "MyWindowClass";
 
-    WNDCLASSEX wc = {0};
-    wc.cbSize        = sizeof(WNDCLASSEX);
-    wc.lpfnWndProc   = WindowProc;
-    wc.hInstance     = hInstance;
-    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    // Inisialisasi struktur kelas jendela
+    WNDCLASSEX wc;
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc.lpszClassName = szClassName;
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = className;
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
+    // Registrasi kelas jendela
     if (!RegisterClassEx(&wc)) {
-        MessageBox(NULL, L"Window Registration Failed!", L"Error", MB_ICONEXCLAMATION | MB_OK);
+        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
 
-    hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, szClassName, szWindowName, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, hInstance, NULL);
-
+    // Membuat jendela baru
+    HWND hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, className, "My Window", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 480, 320, NULL, NULL, hInstance, NULL);
     if (hwnd == NULL) {
-        MessageBox(NULL, L"Window Creation Failed!", L"Error", MB_ICONEXCLAMATION | MB_OK);
+        MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
 
+    // Menampilkan jendela
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
+    // Message loop
+    MSG msg;
     while (GetMessage(&msg, NULL, 0, 0) > 0) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    return msg.wParam;
+    return (int)msg.wParam;
 }
 
+// Definisi fungsi callback
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+
+    return 0;
+}
